13cd4f572fdbb7c5f7709cf01c59e80307f37e57
aakrut <aakrutdabhi@gmail.com>
2022-08-01 18:40:56 +0530

    added-home-page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["image.tmdb.org"],
+  },
+  env: {
+    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+  },
+};
