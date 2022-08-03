a568cc1e38c93d06b2ce2ab78fa022841db2de4a
aakrut <aakrutdabhi@gmail.com>
2022-08-03 11:17:56 +0530

    scrolling-fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,11 @@ const Home = ({
 
   if (loading) return "Loading!...";
   return (
-    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh] w-full ">
+    <div
+      className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
+        showModal && "!h-screen overflow-hidden"
+      }`}
+    >
       <Head>
         <title>Netflix</title>
         <link rel="icon" href="/favicon.io" />
